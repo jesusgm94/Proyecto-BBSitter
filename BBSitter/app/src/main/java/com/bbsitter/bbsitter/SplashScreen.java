2246ab83f25c0bc505be3b62f222d8d66bc59114
jesusgm94 <58287924+jesusgm94@users.noreply.github.com>
2020-09-22 13:02:43 +0200

    Aplicacion

---
+++ BBSitter/app/src/main/java/com/bbsitter/bbsitter/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.bbsitter.bbsitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.bbsitter.bbsitter.MainActivity;
+import com.bbsitter.bbsitter.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        /*Ocultamos Actionbar*/
+        //getSupportActionBar().hide();
+
+        TimerTask task=new TimerTask() {
+            @Override
+            public void run() {
+                Intent i=new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        };
+
+        Timer timer=new Timer();
+        timer.schedule(task, 2000);
+    }
+}
