3717c06845b5e6f513d17c969609ad5591abd058
Jesus GM <jesusglezmtz.94@gmail.com>
2020-10-29 15:35:12 +0100

    Login hecho, registro hecho

--- BBSitter/app/src/main/java/com/bbsitter/bbsitter/RegistroUsuarioActivity.java
+++ BBSitter/app/src/main/java/com/bbsitter/bbsitter/RegistroUsuarioActivity.java
@@ -104,7 +104,7 @@ public class RegistroUsuarioActivity extends AppCompatActivity {
         password = registroPass.getEditText().getText().toString().trim();
 
         if (password.isEmpty()) {
-            registroPass.setError("Debes rellenar el campoo");
+            registroPass.setError("Debes rellenar el campo");
             Toast.makeText(RegistroUsuarioActivity.this, "Debes rellenar el campo contraseña", Toast.LENGTH_LONG).show();
             return false;
         }
