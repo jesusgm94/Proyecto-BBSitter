2227a80da89fb905588cb981373affb7856c61e8
Jesús GM <jesusglezmtz.94@gmail.com>
2020-11-11 18:34:00 +0100

    Guardadas las fotos de los perfiles en firebase y puesto en el JSON de cada perfil

--- BBSitter/app/src/main/java/com/bbsitter/bbsitter/Main/MainActivity.java
+++ BBSitter/app/src/main/java/com/bbsitter/bbsitter/Main/MainActivity.java
@@ -28,21 +28,24 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        /*fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own actio", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+        /*Menú Usuario*/
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+        /*Menu izquierda*/
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_fav, R.id.nav_mensajes)
                 .setDrawerLayout(drawer)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
