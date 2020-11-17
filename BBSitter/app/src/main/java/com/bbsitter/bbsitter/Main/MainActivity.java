263abf23842c7115d8ca4a7cca5c96dd7e29fa45
Jesús GM <jesusglezmtz.94@gmail.com>
2020-11-17 15:45:58 +0100

    Guardadas las fotos de los perfiles en firebase y puesto en el JSON de cada perfil, Cambiar pass hecho

--- BBSitter/app/src/main/java/com/bbsitter/bbsitter/Main/MainActivity.java
+++ BBSitter/app/src/main/java/com/bbsitter/bbsitter/Main/MainActivity.java
@@ -3,6 +3,7 @@ package com.bbsitter.bbsitter.Main;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -18,6 +19,8 @@ import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
@@ -31,36 +34,73 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        /*fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own actio", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
 
-        /*Menú Usuario*/
-        NavigationView navigationView = findViewById(R.id.nav_view);
-
-        /*Menu izquierda*/
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_fav, R.id.nav_mensajes)
-                .setDrawerLayout(drawer)
-                .build();
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        mAuth = FirebaseAuth.getInstance();
 
 
+        Random r = new Random();
+        int i = r.nextInt(1);
 
 
+        Toast.makeText(this, i, Toast.LENGTH_SHORT).show();
+
+        if (i == 1) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+            FloatingActionButton fab = findViewById(R.id.fab);
+            /*fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Replace with your own actio", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });*/
+            DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+            /*Menú Usuario*/
+            NavigationView navigationView = findViewById(R.id.nav_view);
+
+            /*Menu izquierda*/
+            mAppBarConfiguration = new AppBarConfiguration.Builder(
+                    R.id.nav_home, R.id.nav_fav, R.id.nav_mensajes)
+                    .setDrawerLayout(drawer)
+                    .build();
+
+            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+            NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+            NavigationUI.setupWithNavController(navigationView, navController);
+
+
+        } else if (i == 0) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+            FloatingActionButton fab = findViewById(R.id.fab);
+            /*fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Replace with your own actio", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });*/
+            DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+            /*Menú Usuario*/
+            NavigationView navigationView = findViewById(R.id.nav_view);
+
+            /*Menu izquierda*/
+            mAppBarConfiguration = new AppBarConfiguration.Builder(
+                    R.id.nav_home, R.id.nav_fav, R.id.nav_mensajes)
+                    .setDrawerLayout(drawer)
+                    .build();
+
+            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+            NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+            NavigationUI.setupWithNavController(navigationView, navController);
+        }
 
 
     }
