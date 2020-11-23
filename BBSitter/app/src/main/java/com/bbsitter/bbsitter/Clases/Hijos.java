26247a76f776f85323d2a3513e65ae7f95733303
Jesus GM <jesusglezmtz.94@gmail.com>
2020-11-23 21:15:48 +0100

    Cargar imagenes, nombre y direccion de familia en item hecho Intentando OnClickListener en item de RecyclerView Maquetando PerfilFamilia

---
+++ BBSitter/app/src/main/java/com/bbsitter/bbsitter/Clases/Hijos.java
@@ -0,0 +1,16 @@
+package com.bbsitter.bbsitter.Clases;
+
+import java.io.Serializable;
+
+public class Hijos implements Serializable {
+
+    String imgHijos;
+    String nombreHijos;
+    String edadHijos;
+    String alergiasHijos;
+    String otrosDatosHijos;
+
+
+
+
+}
