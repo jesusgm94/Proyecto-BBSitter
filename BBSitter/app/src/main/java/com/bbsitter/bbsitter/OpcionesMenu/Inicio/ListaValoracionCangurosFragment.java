070a3758b3c1d97f9b339fc0671aff13b9b56b68
David <davidarribas1984@gmail.com>
2020-12-02 12:45:57 +0100

    Eliminar anuncios, hijos y perfiles (conseguido) Cerrar sesion (conseguido) Controlar errores de los TextView al crear anuncios, hijos, canguros, familias...(Falta en canguro) Editar perfiles (conseguido) Cargar los pluses, idiomas y prefEdades en los perfiles Canguro ( chips dinamicos) Cargar rating a los canguros. Creado fragment para listar por valoracion (no me acuerdo si funcionaba del todo)
    POR HACER:
    Arreglar cambio de foto en Navigation al editar perfil ( opcional )
    Más filtros en TAB canguros
    Chips dinámicos en familia
    Eliminar anuncios
    Eliminar hijos ( opcional)
    Boton google
    El usuario registrado entra sin login

--- BBSitter/app/src/main/java/com/bbsitter/bbsitter/OpcionesMenu/Inicio/ListaValoracionCangurosFragment.java
+++ BBSitter/app/src/main/java/com/bbsitter/bbsitter/OpcionesMenu/Inicio/ListaValoracionCangurosFragment.java
@@ -1,19 +1,15 @@
 package com.bbsitter.bbsitter.OpcionesMenu.Inicio;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.bbsitter.bbsitter.Adaptadores.CanguroAdapter;
 import com.bbsitter.bbsitter.Clases.Canguro;
@@ -21,33 +17,20 @@ import com.bbsitter.bbsitter.OpcionesMenuCanguro.PerfilCanguroFragment;
 import com.bbsitter.bbsitter.R;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.squareup.picasso.Picasso;
 
 import java.util.Random;
 
-import static android.content.Context.LOCATION_SERVICE;
-
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ListaCangurosFragment#newInstance} factory method to
+ * Use the {@link ListaValoracionCangurosFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ListaCangurosFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class ListaValoracionCangurosFragment extends Fragment {
 
     LatLng MIUBICACION;
-
 
     private FirebaseFirestore bbdd;
     private RecyclerView recyclerViewListaCanguros;
@@ -57,36 +40,19 @@ public class ListaCangurosFragment extends Fragment {
     Random distancia = new Random();
 
 
-    public ListaCangurosFragment() {
+    public ListaValoracionCangurosFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ListaCangurosFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ListaCangurosFragment newInstance(String param1, String param2) {
-        ListaCangurosFragment fragment = new ListaCangurosFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
+    public static ListaValoracionCangurosFragment newInstance(String param1, String param2) {
+        ListaValoracionCangurosFragment fragment = new ListaValoracionCangurosFragment();
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
 
     }
 
@@ -96,37 +62,17 @@ public class ListaCangurosFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_lista_canguros, container, false);
 
-        // OBETENMOS LA UBICACION PARA CALCULAR LA DISTANCIA DE LOS CANGUROS *************
-/*
-        LocationManager locationManager;
-        locationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
-
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(getActivity(), new String[]{
-
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-
-            },1000);
-        }
-
-        Location loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        MIUBICACION = new LatLng(loc.getLatitude(), loc.getLongitude());
-*/
-
         // RECYCLER VIEW ****************
-
-        recyclerViewListaCanguros = view.findViewById(R.id.recycler_ListaCanguros);
+        recyclerViewListaCanguros = view.findViewById(R.id.recycler_ListaValoracionCanguros);
         recyclerViewListaCanguros.setLayoutManager(new LinearLayoutManager(getContext()));
         bbdd = FirebaseFirestore.getInstance();
 
 
-        Query query = bbdd.collection("canguros");
+        Query query = bbdd.collection("canguros").orderBy("rating", Query.Direction.DESCENDING);
 
         FirestoreRecyclerOptions<Canguro> firestoreRecyclerOptions = new FirestoreRecyclerOptions.Builder<Canguro>()
                 .setQuery(query, Canguro.class).build();
 
-        //mAdapter = new CanguroAdapter(firestoreRecyclerOptions);
         mAdapter = new CanguroAdapter(firestoreRecyclerOptions) {
             @Override
             protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull Canguro canguro) {
@@ -144,7 +90,7 @@ public class ListaCangurosFragment extends Fragment {
                 holder.getDistancia().setText(dist + " kms");
 
                 final String uid = canguro.getUid();
-                
+
 
                 // Obtenemos el cardview de itemCanguro que hemos instanciado en el onBindViewHolder de AdapterCangruo
                 holder.getCardViewCanguro().setOnClickListener(new View.OnClickListener() {
@@ -170,6 +116,7 @@ public class ListaCangurosFragment extends Fragment {
 
         mAdapter.notifyDataSetChanged();
         recyclerViewListaCanguros.setAdapter(mAdapter);
+
 
 
         return view;
--- BBSitter/app/src/main/java/com/bbsitter/bbsitter/PagerAdapter.java
+++ BBSitter/app/src/main/java/com/bbsitter/bbsitter/PagerAdapter.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import com.bbsitter.bbsitter.OpcionesMenu.Inicio.ListaCangurosFragment;
+import com.bbsitter.bbsitter.OpcionesMenu.Inicio.ListaValoracionCangurosFragment;
 import com.bbsitter.bbsitter.OpcionesMenuCanguro.Inicio.MapsFragmentCanguros;
 
 public class PagerAdapter  extends FragmentStateAdapter {
@@ -20,7 +21,7 @@ public class PagerAdapter  extends FragmentStateAdapter {
             case 0:
                 return new ListaCangurosFragment();
             case 1:
-                return new ListaCangurosFragment();
+                return new ListaValoracionCangurosFragment();
             case 2:
                 return new ListaCangurosFragment();
             default:
