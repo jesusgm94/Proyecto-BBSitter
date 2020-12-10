f734e940ab2d4102c89b0cfc6ace1f5de009a024
Jesus GM <jesusglezmtz.94@gmail.com>
2020-12-10 21:25:22 +0100

    Link a Git Hub, si no tiene tlf sacamos Snackbar

--- BBSitter/app/src/main/java/com/bbsitter/bbsitter/OpcionesMenuCanguro/Inicio/InicioCanguroFragment.java
+++ BBSitter/app/src/main/java/com/bbsitter/bbsitter/OpcionesMenuCanguro/Inicio/InicioCanguroFragment.java
@@ -76,7 +76,6 @@ public class InicioCanguroFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.inicio_canguro_fragment, container, false);
 
 
